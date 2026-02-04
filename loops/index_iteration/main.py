9442a14b546f585a4ccf5a13df5813f80d526434
Kenfortney <>
2026-02-04 18:41:57 +0000

    Index Iteration

---
+++ loops/index_iteration/main.py
@@ -0,0 +1,12 @@
+prices = [29.99, 45.50, 12.75, 38.20]
+
+for cost in range(len(prices)):
+    if cost == 0:
+        prices[cost] -= prices[cost] * .10
+    elif cost == 1:
+        prices[cost] -= prices[cost] * .20
+    elif cost == 2:
+        prices[cost] -= prices[cost] * .15
+    elif cost == 3:
+        prices[cost] -= prices[cost] * .05
+    print(f"Updated price for item {cost}: ${prices[cost]:.2f}")
